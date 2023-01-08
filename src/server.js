1841419c3ba03858d0246f4a7ca3d7d85896f8dd
MytsV <mytsvictoria@gmail.com>
2023-01-08 21:59:16 +0200

    basic work with db implemented

--- src/server.js
+++ src/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {MongoClient} = require("mongodb");
 const port = 8080;
+
+const client = new MongoClient("mongodb://127.0.0.1:27017");
 
 const app = express();
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const router = new express.Router();
+router.get('/api', async (req, res) => {
+  await client.connect();
+  const result = await client.db('pixel-toons').collection('users').find({}).toArray();
+  res.status(200).send(result);
+});
+
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Started on http://localhost:${port}`);
