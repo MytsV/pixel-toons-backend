1b497d496543c33df2ebe85f448e844879356ff1
MytsV <mytsvictoria@gmail.com>
2023-01-08 22:22:13 +0200

    user model created and mongoose connected

--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {MongoClient} = require("mongodb");
+const {MongoClient} = require('mongodb');
 const port = 8080;
 
-const client = new MongoClient("mongodb://127.0.0.1:27017");
+const client = new MongoClient('mongodb://127.0.0.1:27017');
 
 const app = express();
 app.use(bodyParser.text());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+const db = require('./models');
+
+db.mongoose
+    .connect(`mongodb://127.0.0.1:27017`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Successfully connect to MongoDB.');
+    })
+    .catch((err) => {
+      console.error('Connection error', err);
+      process.exit();
+    });
 
 const router = new express.Router();
 router.get('/api', async (req, res) => {
