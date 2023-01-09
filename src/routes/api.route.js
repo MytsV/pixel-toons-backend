1e1a18fca2870eac92400cafabb4cb30cf4f0098
MytsV <mytsvictoria@gmail.com>
2023-01-09 19:50:52 +0200

    file downloading finished

--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {signUp, signIn} = require("../controllers/auth.controller");
 const {tokenToID} = require("../controllers/auth.test.controller");
 const {editUser} = require("../controllers/edit.controller");
-const {uploadFile} = require("../controllers/static_file.controller");
+const {uploadFile, downloadFile} = require("../controllers/static_file.controller");
 const router = new express.Router();
 
 router.post('/signup', signUp);
@@ -13,5 +13,6 @@ router.get('/test/validate', tokenToID);
 router.post('/edit-user/:id', editUser);
 
 router.post('/upload-file', uploadFile);
+router.get('/uploads/:name', downloadFile);
 
 module.exports = router;
