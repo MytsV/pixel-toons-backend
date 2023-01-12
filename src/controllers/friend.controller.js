b0b269636bb45e7320ddb4c73f70fa4d94204e1e
MytsV <mytsvictoria@gmail.com>
2023-01-12 12:26:57 +0200

    friends refactored and bugs fixed

--- src/controllers/friend.controller.js
+++ src/controllers/friend.controller.js
@@ -22,6 +22,10 @@ const addFriend = async (req, res) => {
   }
   if (!friend) return sendMsg(res, 'friend_not_found', 404);
 
+  if (user._id.equals(friend._id)) {
+    return sendMsg(res, 'cannot_add_self', 422);
+  }
+
   if (user.data.friends.includes(friend._id)) {
     return sendMsg(res, 'friend_already_in', 422);
   }
@@ -56,6 +60,38 @@ const getFriends = async (req, res) => {
   ));
 };
 
-//TODO: deletion
+const deleteFriend = async (req, res) => {
+  if (!req.query.friendId) {
+    // 422 Unprocessable Entity
+    return sendMsg(res, 'friend_id_missing', 422);
+  }
 
-module.exports = { addFriend, getFriends };
+  validateToken(req, res);
+  if (!req.userId) return;
+
+  const user = await User.findOne({ _id: req.userId });
+  if (!user) return sendMsg(res, 'user_not_found', 404);
+
+  let friend;
+  try {
+    friend = await User.findOne({_id: req.query.friendId});
+  } catch (e) {
+    return sendMsg(res, 'friend_not_found', 404);
+  }
+  if (!friend) return sendMsg(res, 'friend_not_found', 404);
+
+  if (!user.data.friends.includes(friend._id)) {
+    return sendMsg(res, 'friend_not_in', 422);
+  }
+
+  const newFriends = user.data.friends.filter((u) => !u._id.equals(friend._id));
+  await user.updateOne({
+    data: {
+      friends: newFriends,
+    },
+  });
+
+  return sendMsg(res, 'friend_deleted', 200);
+};
+
+module.exports = { addFriend, getFriends, deleteFriend };
--- src/routes/api.route.js
+++ src/routes/api.route.js
@@ -4,7 +4,7 @@ const { tokenToID } = require('../controllers/auth.test.controller');
 const { editUser } = require('../controllers/user-edit.controller');
 const { uploadFile, downloadFile } = require('../controllers/static_file.controller');
 const { getAll, getById } = require('../controllers/user.controller');
-const { addFriend, getFriends } = require('../controllers/friend.controller');
+const { addFriend, getFriends, deleteFriend } = require('../controllers/friend.controller');
 const router = new express.Router();
 
 router.post('/signup', signUp)
@@ -22,5 +22,6 @@ router.get('/user', getAll)
 
 router.get('/friend', getFriends);
 router.post('/friend', addFriend);
+router.delete('/friend', deleteFriend);
 
 module.exports = router;
