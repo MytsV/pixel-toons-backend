c26d30302d1dec81a17f4fe27fd53e4800d15d8d
MytsV <mytsvictoria@gmail.com>
2023-01-12 13:30:38 +0200

    post model created

---
+++ src/models/post.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Post = mongoose.model(
+  'Post',
+  new mongoose.Schema({
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    name: {
+      type: String,
+      required: true,
+      minLength: 5,
+      maxLength: 20,
+    },
+    desc: {
+      type: String,
+      minLength: 5,
+      maxLength: 140,
+    },
+    url: {
+      type: String,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+  }),
+  'posts',
+);
+
+module.exports = Post;
