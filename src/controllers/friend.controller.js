a4d6ec24f115f2ad622adbba9466289d0ccacb7e
MytsV <mytsvictoria@gmail.com>
2023-01-12 12:01:28 +0200

    friend controller refactored, bugs fixed

--- src/controllers/friend.controller.js
+++ src/controllers/friend.controller.js
@@ -1,15 +1,30 @@
 const { validateToken } = require('../middleware');
 const User = require('../models/user.model');
+const sendMsg = require("../middleware/message_builder");
 
 const addFriend = async (req, res) => {
-  if (!req.query.friendId) return res.status(422).send('Parameter missing');
+  if (!req.query.friendId) {
+    // 422 Unprocessable Entity
+    return sendMsg(res, 'friend_id_missing', 422);
+  }
+
   validateToken(req, res);
   if (!req.userId) return;
-  const user = await User.findOne({ _id: req.userId });
-  if (!user) return res.status(404).send('User not found');
 
-  const friend = await User.findOne({ _id: req.query.friendId });
-  if (!friend) return res.status(404).send('Friend not found');
+  const user = await User.findOne({ _id: req.userId });
+  if (!user) return sendMsg(res, 'user_not_found', 404);
+
+  let friend;
+  try {
+    friend = await User.findOne({_id: req.query.friendId});
+  } catch (e) {
+    return sendMsg(res, 'friend_not_found', 404);
+  }
+  if (!friend) return sendMsg(res, 'friend_not_found', 404);
+
+  if (user.data.friends.includes(friend._id)) {
+    return sendMsg(res, 'friend_already_in', 422);
+  }
 
   const newFriends = [...user.data.friends];
   newFriends.push(friend._id);
@@ -19,13 +34,17 @@ const addFriend = async (req, res) => {
     },
   });
 
-  return res.send('Successfully added a friend');
+  return sendMsg(res, 'friend_added', 200);
 };
 
 const getFriends = async (req, res) => {
-  if (!req.query.id) return res.status(422).send('Parameter missing');
+  if (!req.query.id) {
+    // 422 Unprocessable Entity
+    return sendMsg(res, 'id_missing', 422);
+  }
+
   const user = await User.findOne({ _id: req.query.id });
-  if (!user) return res.status(404).send('User not found');
+  if (!user) return sendMsg(res, 'user_not_found', 404);
   const friends = await User.find({ _id: { $in: user.data.friends } });
   res.send(friends.map((u) =>
     //TODO: extract
