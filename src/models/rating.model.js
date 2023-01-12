0c339675cae0db3da458888c338efe0d82458bbd
MytsV <mytsvictoria@gmail.com>
2023-01-12 15:22:48 +0200

    rating model created

---
+++ src/models/rating.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Rating = mongoose.model(
+  'Rating',
+  new mongoose.Schema({
+    postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    upvote: {
+      type: Boolean,
+      required: true,
+    },
+  }),
+  'ratings',
+);
+
+module.exports = Rating;
