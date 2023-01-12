31d55d2a18a93517a88437bfd5a63bfcc1293214
MytsV <mytsvictoria@gmail.com>
2023-01-12 15:23:07 +0200

    fixed required parameters

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Post = mongoose.model(
   'Post',
   new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     name: {
       type: String,
       required: true,
