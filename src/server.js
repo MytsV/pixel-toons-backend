c3b3ecc321e4dd14792013d5be84e17ded05e711
MytsV <mytsvictoria@gmail.com>
2023-01-12 12:46:21 +0200

    added error handling for mongoose db

--- src/server.js
+++ src/server.js
@@ -8,9 +8,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 const db = require('./models');
-
-// TODO: move and error handling
-db.mongoose.connect(process.env.DB_URL);
+db.mongoose.connect(process.env.DB_URL)
+  .catch((err) => {
+    console.log(err);
+    process.exit();
+  });
 
 app.use('/api', require('./routes/api.route'));
 
